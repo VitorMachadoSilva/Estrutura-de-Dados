d61edb621612bcd2c6e340a21088e95706d1e466
Vitor <vitor.machado.silva05@gmail.com>
2024-08-22 08:40:17 -0300

    Aula 04

---
+++ Aula4/Aula4.py
@@ -0,0 +1,9 @@
+# CONTAGEM REGRESSIVA USANDO (FOR RANGE)
+
+contagem_Regressiva = 10
+
+for item in range (1, 11):
+    print(f"Contagem: {contagem_Regressiva}")
+    contagem_Regressiva = contagem_Regressiva - 1 
+else:
+    print("Feliz Ano Novo")
